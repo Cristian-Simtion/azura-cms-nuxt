fb1252cae740572b18e1174e74ac56cb323bb6fd
Cristian-Simtion <cristian.simtion@gmail.com>
2023-06-24 01:23:03 +0300

    2 files modified, 3 added, 1 deleted

---
+++ components/block/row.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">ROW XXXXXXXXXXXXXXXX</div>
+  </div>
+</template>
+
+<style>
+.container {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.container.boxed {
+  max-width: 1264px;
+}
+
+.row {
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 16px;
+}
+
+.row .row {
+  margin-left: -8px;
+  margin-right: -8px;
+}
+
+.row:after {
+  content: '';
+  display: table;
+  clear: both;
+}
+
+@media (min-width: 600px) {
+  .container {
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+}
+
+@media (min-width: 960px) {
+  .container {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,28 @@
+<script setup>
+const getComponent = {
+  row: resolveComponent('Row'),
+};
+
+const blocksList = {
+  blocks: [
+    {
+      name: 'row',
+      blocks: [],
+    },
+    {
+      name: 'row',
+      blocks: [],
+    },
+  ],
+};
+</script>
+
 <template>
   <h1>Home</h1>
+
+  <component
+    v-for="block in blocksList.blocks"
+    :is="getComponent[block.name]"
+    :block="block"
+  ></component>
 </template>
